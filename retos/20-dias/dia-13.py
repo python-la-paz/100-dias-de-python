5c3b4f773f1f5aba80276f839de793956339ec37
taicoding <tai.delgadillo@gmail.com>
2022-05-04 00:06:59 -0400

    dia 13 y 14

---
+++ retos/20-dias/dia-13.py
@@ -0,0 +1,6 @@
+"""
+            Bienvenido al día 13 de #100diasdepython
+                    El reto de hoy es:
+Declara dos variables booleanas, evalúa una expresión lógica
+    usando dos operadores lógicos e imprime el resultado.
+"""
---
+++ soluciones/20-dias/dia-13.py
@@ -0,0 +1,11 @@
+"""
+            Bienvenido al día 13 de #100diasdepython
+                    El reto de hoy es:
+Declara dos variables booleanas, evalúa una expresión lógica
+    usando dos operadores lógicos e imprime el resultado.
+"""
+verdad = True
+falso = False
+expresion = not verdad and falso
+print(expresion)
+# Resultado: False
